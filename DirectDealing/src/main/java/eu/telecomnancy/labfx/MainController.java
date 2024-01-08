4553acccb133fbae0ba5da88dfd510e0d85e2821
JEANJACQUOT Thomas <thomas.jeanjacquot@telecomnancy.eu>
2024-01-08 14:05:07 +0100

    WelcomPage qui s'adapte à la taille de la fenetre

--- DirectDealing/src/main/java/eu/telecomnancy/labfx/MainController.java
+++ DirectDealing/src/main/java/eu/telecomnancy/labfx/MainController.java
@@ -1,33 +1,42 @@
 package eu.telecomnancy.labfx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.application.Platform;
+
+import java.io.IOException;
 
 public class MainController {
 
-private static Stage primaryStage;
-
-    public static void setPrimaryStage(Stage stage) {
-        primaryStage = stage;
-    }
-
-    public static Stage getPrimaryStage() {
-        return primaryStage;
-    }
+    @FXML
+    private AnchorPane mainAnchorPane;
 
     @FXML
-    public void handleRetour() {
-        Platform.exit();
+    public void initialize() {
+        // Charger le contenu de WelcomePage.fxml
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/WelcomePage.fxml"));
+            Parent welcomePage = loader.load();
+
+            // Ajouter le WelcomePage à l'AnchorPane parent
+            mainAnchorPane.getChildren().add(welcomePage);
+
+            // Configurer les contraintes pour que le WelcomePage remplisse l'AnchorPane
+            AnchorPane.setTopAnchor(welcomePage, 0.0);
+            AnchorPane.setBottomAnchor(welcomePage, 0.0);
+            AnchorPane.setLeftAnchor(welcomePage, 0.0);
+            AnchorPane.setRightAnchor(welcomePage, 0.0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @FXML
-    public void handleInscription() {
-    }
+    // Autres méthodes du contrôleur, si nécessaire
 
-    @FXML
-    public void chooseImage() {
-    
+    // Exemple de méthode pour accéder au Stage depuis le contrôleur
+    public void setPrimaryStage(Stage primaryStage) {
+        Main.setCurrentStage(primaryStage);
     }
-
 }
