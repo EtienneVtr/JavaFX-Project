d95df7fa87b01c3d08a3451abafded61a763a077
VATRY Etienne <etienne.vatry@telecomnancy.eu>
2024-01-09 09:48:48 +0100

    Début offres équipements et services

---
+++ DirectDealing/src/main/java/eu/telecomnancy/labfx/EquipmentController.java
@@ -0,0 +1,13 @@
+package eu.telecomnancy.labfx;
+
+import javafx.fxml.FXML;
+
+public class EquipmentController {
+    @FXML public void handleCreateOffer() {
+        System.out.println("Create offer");
+    }
+
+    @FXML public void handleConsultOffer() {
+        System.out.println("Consult offer");
+    }
+}
