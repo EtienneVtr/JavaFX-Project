fa028d7c186f1e52b5c133cff28ee2dbbed74b5f
Celestin.Morel <celestin.morel@telecomnancy.eu>
2024-01-09 14:55:15 +0100

    commit pour pouvoir pull c e que jiji a fait

--- DirectDealing/src/main/java/eu/telecomnancy/labfx/Main.java
+++ DirectDealing/src/main/java/eu/telecomnancy/labfx/Main.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static User user;
     private static Stage primaryStage;
 
     public static Stage getPrimaryStage() {
